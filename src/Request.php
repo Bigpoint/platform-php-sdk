271f031ae93b4e71ae411f806e8684597168a7ee
root <root@rechenknecht.gebeat.info>
2012-11-15 21:48:32 +0100

    complete request class

--- src/Request.php
+++ src/Request.php
@@ -4,4 +4,86 @@ namespace Bigpoint;
 
 class Request
 {
+    /**
+     * @var string
+     */
+    private $method;
+
+    /**
+     * @var string
+     */
+    private $uri;
+
+    /**
+     * @var array
+     */
+    private $payload = array();
+
+    /**
+     * Set the method.
+     *
+     * @param string $method
+     */
+    public function setMethod($method)
+    {
+        $this->method = $method;
+    }
+
+    /**
+     * Set the uri.
+     *
+     * @param string $uri
+     */
+    public function setUri($uri)
+    {
+        $this->uri = $uri;
+    }
+
+    /**
+     * Set the parameters.
+     *
+     * @param array $parameters
+     */
+    public function setParameters(array $parameters)
+    {
+        $this->payload = $parameters;
+    }
+
+    /**
+     * Clear the properties.
+     */
+    public function flush()
+    {
+        $this->method = null;
+        $this->uri = null;
+        $this->payload = array();
+    }
+
+    /**
+     * Get the method.
+     *
+     * @return string
+     */
+    public function getMethod()
+    {
+        return $this->method;
+    }
+
+    /**
+     * Get the uri.
+     *
+     * @return string
+     */
+    public function getUri()
+    {
+        return $this->uri;
+    }
+
+    /**
+     * Get the body.
+     */
+    public function getBody()
+    {
+        return http_build_query($this->payload, '', '&');
+    }
 }
