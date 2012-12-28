5237b7a39ca2f06a2d73cb9275886466581557e6
root <root@rechenknecht.gebeat.info>
2012-12-28 20:34:49 +0100

    add unit test for environment

---
+++ tests/EnvironmentTest.php
@@ -0,0 +1,181 @@
+<?php
+
+namespace Bigpoint;
+
+class EnvironmentTest extends \PHPUnit_Framework_TestCase
+{
+    /**
+     * @var Environment
+     */
+    private $objectUnderTest;
+
+    /**
+     * @var string
+     */
+    const INVALID_KEY = 'INVALIDKEY';
+
+    /**
+     * @var string
+     */
+    const DEFAULT_VALUE = 'DEFAULTVALUE';
+
+    /**
+     * @var string
+     */
+    const REQUEST_URI = '/site';
+
+    /**
+     * @var string
+     */
+    const REQUEST_URI_WITH_QUERY = '/site?referrer=partner';
+
+    /**
+     * @var string
+     */
+    const HTTP_HOST = 'app.example.com';
+
+    /**
+     * @var string
+     */
+    const REQUEST_URI_EMPTY = '';
+
+    public function superGlobalsProvider()
+    {
+        return array(
+            array(
+                array(
+                    'foo' => 'bar',
+                    'php' => 'hypertext processor',
+                ),
+                array(
+                    'DOCUMENT_ROOT' => '/var/www/default',
+                    'SERVER_PROTOCOL' => 'HTTP/1.1',
+                ),
+            ),
+        );
+    }
+
+    /**
+     * @dataProvider superGlobalsProvider
+     *
+     * @param array $get
+     * @param array $server
+     */
+    public function testGetGetParam($get, $server)
+    {
+        $this->objectUnderTest = new Environment($get, $server);
+        foreach ($get as $key => $expected) {
+            $actual = $this->objectUnderTest->getGetParam($key);
+            $this->assertSame($expected, $actual);
+        }
+    }
+
+    public function testGetGetParamWithInvalidKey()
+    {
+        $this->objectUnderTest = new Environment(array(), array());
+        $actual = $this->objectUnderTest->getGetParam(self::INVALID_KEY);
+
+        $this->assertNull($actual);
+    }
+
+    public function testGetGetParamWithInvalidKeyAndDefaultValue()
+    {
+        $this->objectUnderTest = new Environment(array(), array());
+        $actual = $this->objectUnderTest->getGetParam(
+            self::INVALID_KEY,
+            self::DEFAULT_VALUE
+        );
+
+        $this->assertSame(self::DEFAULT_VALUE, $actual);
+    }
+
+    /**
+     * @dataProvider superGlobalsProvider
+     *
+     * @param array $get
+     * @param array $server
+     */
+    public function testGetServerParam($get, $server)
+    {
+        $this->objectUnderTest = new Environment($get, $server);
+        foreach ($server as $key => $expected) {
+            $actual = $this->objectUnderTest->getServerParam($key);
+            $this->assertSame($expected, $actual);
+        }
+    }
+
+    public function testGetServerParamWithInvalidKey()
+    {
+        $this->objectUnderTest = new Environment(array(), array());
+        $actual = $this->objectUnderTest->getServerParam(self::INVALID_KEY);
+
+        $this->assertNull($actual);
+    }
+
+    public function testGetServerParamWithInvalidKeyAndDefaultValue()
+    {
+        $this->objectUnderTest = new Environment(array(), array());
+        $actual = $this->objectUnderTest->getServerParam(
+            self::INVALID_KEY,
+            self::DEFAULT_VALUE
+        );
+
+        $this->assertSame(self::DEFAULT_VALUE, $actual);
+    }
+
+    public function testGetDocumentURI()
+    {
+        $this->objectUnderTest = new Environment(
+            array(),
+            array(
+                'REQUEST_URI' => self::REQUEST_URI_WITH_QUERY,
+            )
+        );
+        $actual = $this->objectUnderTest->getDocumentURI();
+
+        $this->assertSame(self::REQUEST_URI, $actual);
+    }
+
+    public function testGetDocumentURIWithEmptyRequestUri()
+    {
+        $this->objectUnderTest = new Environment(
+            array(),
+            array(
+                'REQUEST_URI' => self::REQUEST_URI_EMPTY,
+            )
+        );
+        $actual = $this->objectUnderTest->getDocumentURI();
+
+        $this->assertSame(self::REQUEST_URI_EMPTY, $actual);
+    }
+
+    public function testGetCurrentURIWithHTTP()
+    {
+        $this->objectUnderTest = new Environment(
+            array(),
+            array(
+                'HTTP_HOST' => self::HTTP_HOST,
+                'HTTPS' => null,
+            )
+        );
+        $actual = $this->objectUnderTest->getCurrentURI();
+        $expected = 'http://' . self::HTTP_HOST;
+
+        $this->assertSame($expected, $actual);
+    }
+
+    public function testGetCurrentURIWithHTTPS()
+    {
+        $this->objectUnderTest = new Environment(
+            array(),
+            array(
+                'HTTP_HOST' => self::HTTP_HOST,
+                'HTTPS' => true,
+            )
+        );
+        $actual = $this->objectUnderTest->getCurrentURI();
+        $expected = 'https://' . self::HTTP_HOST;
+
+        $this->assertSame($expected, $actual);
+    }
+}
